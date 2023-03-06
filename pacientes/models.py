567ddf499fa7bb5b8496d38e949e3a218a6b259a
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2023-03-05 22:32:37 -0600

    Creacion de modelos en postgresql

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,16 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
-class Hospital(models.Model):
-    rfc_hospital = models.CharField(max_length=10, primary_key=True, null=False)
-    cp_hospital = models.CharField(max_length=12, null=False)
-    nombre_hospital = models.CharField(max_length=30, null=False)
-    telefono_hospital = models.CharField(max_length=15, null=False)
-    direccion_hospital = models.CharField(max_length=250, null=False)
-
-class Medicamentos(models.Model):
-    id_medicamento = models.CharField(max_length=10, primary_key=True, null=False)
+class Medicamento(models.Model):
     laboratorio = models.CharField(max_length=30, null=False)
     nombre_generico = models.CharField(max_length=30, null=False)
     nombre_comercial = models.CharField(max_length=30, null=False)
@@ -18,12 +9,17 @@ class Medicamentos(models.Model):
     forma_farmaceutica = models.CharField(max_length=30, null=False)
     concentracion = models.CharField(max_length=30, null=False)
 
+    class Meta:
+        db_table = "medicamentos"
+        
+
 class Paciente(models.Model):
-    id_paciente = models.CharField(max_length=10, primary_key=True, null=False)
     nombre_paciente = models.CharField(max_length=30, null=False)
     ape_pa_paciente = models.CharField(max_length=30, null=False)
     ape_ma_paciente = models.CharField(max_length=30, null=False)
-    edad_paciente = models.IntegerField(max_length=2, null=False)
+
+    edad_paciente = models.SmallIntegerField(null=False)
+
     peso_paciente = models.FloatField(null=False)
     estatura_paciente = models.FloatField(null=False)
     fecha_naci_paciente = models.DateField()
@@ -33,20 +29,18 @@ class Paciente(models.Model):
     padecimientos_paciente = models.CharField(max_length=50)
     alergias_paciente = models.CharField(max_length=50)
 
-class Pago(models.Model):
-    id_transaccion = models.IntegerField(primary_key=True)
-    descripcion_pago = models.CharField(max_length=100, null=False)
-    monto_pago_caja = models.FloatField(null=False)
-    fecha_pago_caja = models.FloatField(null=False)
-    hora_pago_caja = models.TimeField(null=False)
-    rfc_hospital = models.CharField(max_length=10)
-    rfc_hospital = models.CharField(max_length=10) # esta y la de abajo llevan FK no le entendi bien al pedo 
-    id_paciente = models.CharField(max_length=10)
+    class Meta:
+        db_table = "pacientes"
 
-class RecetaMedica(models.Model):
-    id_receta = models.CharField(max_length=10, primary_key=True, null=False)
+class Receta(models.Model):
     fecha_receta = models.DateField(null=False)
     hora_receta = models.TimeField(null=False)
     dosis_medicamento = models.CharField(max_length=70, null=False)
     id_medicamento = models.CharField(max_length=10)
     id_consulta = models.CharField(max_length=10)
+
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    medicamentos = models.ManyToManyField(Medicamento)
+
+    class Meta:
+        db_table = "recetas"
